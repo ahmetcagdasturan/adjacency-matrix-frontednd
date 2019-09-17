446034a013ffab4bd4d6ebd7439a1f5a43fedb67
Ahmet Çağdaş Turan <ahmetcagdasturan@outlook.com>
2019-09-17 23:09:10 +0300

    Getting adjacency list from api.

--- src/GlobalVariables.js
+++ src/GlobalVariables.js
@@ -1,4 +1,4 @@
-export const URL_BASE = 'http://172.20.10.2:3000';
+export const URL_BASE = 'http://192.168.0.12:3000';
 export const URL_MATRIX = URL_BASE+'/matrix';
 export const URL_ADJANCENCYMATRIX = URL_MATRIX +'/adjacencymatrix';
 export const URL_ADJANCENCYLIST = URL_MATRIX +'/adjacencylist';
