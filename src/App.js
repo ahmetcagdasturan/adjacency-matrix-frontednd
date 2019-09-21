8bc4e03b7a05a9721c103e1cfa2626d5f39246ec
Ahmet Çağdaş Turan <ahmetcagdasturan@outlook.com>
2019-09-21 23:55:58 +0300

    getting list result in graph and refactoring code

--- src/App.js
+++ src/App.js
@@ -1,174 +1,148 @@
 import React, {Component} from 'react';
-//import { Button , View, Text} from 'react-bootstrap';
-import logo from './logo.svg';
 import './App.css';
 import * as Api from './api.js';
-import {ReactCytoscape} from 'react-cytoscape';
-import cytoscape from 'cytoscape';
 import { Graph } from 'react-d3-graph';
-
 
 export default class App extends Component{
 
   constructor(props) {
     super(props);
-    this.state = {data: {
+    this.state = {request: {
                     param: null
                   },
-                  data1: null,
-                  data2: "",
-                  nodess: [],
-                  edgess: [],
-                  data3: null,
-                  data4: "",
+                  matrixTextArea: "",
+                  listTextArea: "",
                   myConfig: {
                     nodeHighlightBehavior: true,
                     node: {
-                        color: 'lightgreen',
+                        fontColor: 'white',
+                        color: 'lightblue',
                         size: 120,
-                        highlightStrokeColor: 'blue'
+                        highlightStrokeColor: 'gray'
                     },
                     link: {
+                        color: "lightgreen",
                         type: "STRAIGHT",
-                        highlightColor: 'lightblue'
+                        highlightColor: 'red'
                     }
-                },
-                data99: {
-                  nodes: [
-                    {id: 'Harry'},
-                    {id: 'Sally'},
-                    {id: 'Alicee'}
-                  ],
-                  links: [
-                      {source: 'Harry', target: 'Sally'},
-                      {source: 'Harry', target: 'Alicee'},
-                      {source: 'Sally', target: 'Alicee'}
-                  ]
-                }
-
+                  },
+                  graphProps: {
+                    nodes: [
+                      {id: '1'},
+                      {id: '2'},
+                      {id: '3'}
+                    ],
+                    links: [
+                        {source: '2', target: '1'},
+                        {source: '2', target: '3'},
+                    ]
+                  }
     };
 
     this.handleChange = this.handleChange.bind(this);
   }
-
-  componentWillMount(){
-    //this.getAdjacencyMatrix();
-  }
   
   handleChange(event) {
-    console.log(event)
-    this.setState({data: {param: event.target.value}});
+    this.setState({request: {param: event.target.value}});
   }
 
-  resetNodesPositions = () => this.refs.graph.resetNodesPositions();
-  restartGraphSimulation = () => this.refs.graph.restartSimulation();
+  setGraphParamsForMatrix(response){
+    var graphParams = {};
+    var nodes = [];
+    var links = [];
+    for(var i=0; i<response.length; i++){
+      nodes.push({id: (i+1).toString(), x: Math.floor(Math.random() * 500), y: Math.floor(Math.random() * 500)})
+      for(var j=0; j<response.length; j++){
+        if(response[i][j]==1){
+          links.push({source: (i+1).toString(), target: (j+1).toString()});
+        }
+      }
+    }
+    graphParams.nodes = nodes;
+    graphParams.links = links;
+    this.setState({graphProps: graphParams});
+  }
 
-  decorateGraphNodesWithInitialPositioning = nodes => {
-    return nodes.map(n =>
-        Object.assign({}, n, {
-            x: n.x || Math.floor(Math.random() * 500),
-            y: n.y || Math.floor(Math.random() * 500),
-        })
-    );
-};
+  setGraphParamsForList(response){
+    var graphParams = {};
+    var nodes = [];
+    var links = [];
+    for(var i=0; i<response.length; i++){
+      nodes.push({id: (i+1).toString(), x: Math.floor(Math.random() * 500), y: Math.floor(Math.random() * 500)})
+      for(var j=0; j<response[i].length; j++){
+        links.push({source: (i+1).toString(), target: response[i][j].toString()});
+      }
+    }
+    graphParams.nodes = nodes;
+    graphParams.links = links;
+    this.setState({graphProps: graphParams});
+  }
 
   getAdjacencyMatrix(){
-    console.log(this.state.data)
-    var denem = this.state.data;
-
-    denem.param = Number(denem.param);
-    console.log(denem)
-    Api.getAdjacencyMatrix(denem)
+    var req = this.state.request;
+    req.param = Number(req.param);
+    Api.getAdjacencyMatrix(req)
       .then( (response) => {
-        var graphParams = {};
-        var nodes = [];
-        var links = [];
-
-        for(var i=0; i<response.length; i++){
-          nodes.push({id: (i+1).toString(), x: Math.floor(Math.random() * 500), y: Math.floor(Math.random() * 500)})
-          for(var j=0; j<response.length; j++){
-            if(response[i][j]==1){
-              var flag = true;
-              for(var t=0; t<links.length; t++){
-                if(links[t].source == (j+1).toString() && links[t].target == (i+1).toString()){
-                  flag = false;
-                }
-              }
-              if(flag){
-                links.push({source: (i+1).toString(), target: (j+1).toString()})
-              }
-            }
-          }
-        }
-        graphParams.nodes = nodes;
-        graphParams.links = links;
-        console.log(graphParams)
-        //this.decorateGraphNodesWithInitialPositioning(graphParams.nodes);
-        console.log(graphParams)
-        this.setState({data99: graphParams});
-        console.log(this.refs)
-        //this.resetNodesPositions();
-        
+        this.setGraphParamsForMatrix(response);
         var stringMatrix = ""
-        for(i=0; i<response.length; i++){
-          for(j=0; j<response.length; j++){
-            stringMatrix += response[i][j].toString();
+        for(var i=0; i<response.length; i++){
+          for(var j=0; j<response.length; j++){
+            stringMatrix += response[i][j].toString() + " ";
           }
-          stringMatrix += " / ";
+          stringMatrix += "\n";
         }
-
-        this.setState({data2:stringMatrix})
+        this.setState({matrixTextArea:stringMatrix})
       })
-    
-
   }
 
   getAdjacencyList(){
-    console.log(this.state.data)
-    var denem = this.state.data;
-
-    denem.param = Number(denem.param);
-    console.log(denem)
-    Api.getAdjacencyList(denem)
+    var req = this.state.request;
+    req.param = Number(req.param);
+    Api.getAdjacencyList(req)
       .then( (response) => {
-
+        this.setGraphParamsForList(response);
         var stringMatrix = ""
         for(var i=0; i<response.length; i++){
           for(var j=0; j<response[i].length; j++){
             stringMatrix += response[i][j].toString() + " ";
           }
-          stringMatrix += "  /  ";
+          stringMatrix += "\n";
         }
-
-        this.setState({data4:stringMatrix})
+        this.setState({listTextArea:stringMatrix})
       })
-    
-
   }
 
   render(){
     return (
         <div className="App">
-            <header className="App-header">
-            
+          <header className="App-header">
             <label>
               Input:
-              <textarea value={this.state.data.param} onChange={this.handleChange} />
+              <textarea value={this.state.request.param} onChange={this.handleChange} />
             </label>
-            <button onClick={() => {this.getAdjacencyMatrix()}}>Button</button>
-            <textarea value={this.state.data2}  />
-            <button onClick={() => {this.getAdjacencyList()}}>ButtonList</button>
-             <textarea value={this.state.data4}/>
-
-          <Graph
-            ref="graph"
-            id='graph-id' 
-            data={this.state.data99}
-            config={this.state.myConfig}>
-          </Graph>
-
+            <div style={{float: 'left',
+                            width: '100%',
+                            padding: '10px'}}>
+              <div style={{float: 'left',
+                            width: '50%',
+                            padding: '10px'}}>
+                <textarea value={this.state.matrixTextArea}/>
+                <button onClick={() => {this.getAdjacencyMatrix()}}>Matrix</button>
+              </div>
+              <div style={{float: 'left',
+                            width: '50%',
+                            padding: '10px'}}>
+                <textarea value={this.state.listTextArea}/>
+                <button onClick={() => {this.getAdjacencyList()}}>List</button>
+              </div>
+            </div>
+            <Graph
+              ref="graph"
+              id='graph-id' 
+              data={this.state.graphProps}
+              config={this.state.myConfig}>
+            </Graph>
           </header>
-          
         </div>
     )}
   
