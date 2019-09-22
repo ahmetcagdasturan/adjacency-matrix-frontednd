bbae4f21d8e1cbb853c4bab31e713b8a6bf136f4
Ahmet Çağdaş Turan <ahmetcagdasturan@outlook.com>
2019-09-22 21:40:24 +0300

    IP adress changed as localhost

--- src/GlobalVariables.js
+++ src/GlobalVariables.js
@@ -1,4 +1,4 @@
-export const URL_BASE = 'http://192.168.0.22:3000';
+export const URL_BASE = 'http://localhost:3000';
 export const URL_MATRIX = URL_BASE+'/matrix';
 export const URL_ADJANCENCYMATRIX = URL_MATRIX +'/adjacencymatrix';
 export const URL_ADJANCENCYLIST = URL_MATRIX +'/adjacencylist';
